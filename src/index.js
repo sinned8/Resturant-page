1fa4fff93efc9f86d26b0457047bb371f087b61e
sinned8 <dennisdust2@gmail.com>
2023-10-27 14:10:13 -0400

    Added all necessary NPM scripts and webpack kits. Started on webpage background and header content.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import './css/global.css'
+// function component() {
+//     const element = document.createElement('div');
+ 
+//    element.innerHTML = 'test'
+//    element.classList.add('content');
+ 
+//     return element;
+//   }
+ 
+//   document.body.appendChild(component());
